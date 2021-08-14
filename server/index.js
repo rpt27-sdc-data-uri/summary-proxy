6dc2939ce2843a9498244a2ccba233652eedaa59
Alonzo Sanchez <alonzosanchez3@gmail.com>
2021-08-14 11:21:55 -0500

    ec2

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const port = 5500;
 const priceServer = 'http://ec2-34-221-235-141.us-west-2.compute.amazonaws.com:3000';
 const titleServer = 'http://13.57.14.144:2002';
 const reviewsServer = 'http://54.183.2.218:4001/books/';
-const summaryServer = 'http://ec2-18-188-135-5.us-east-2.compute.amazonaws.com:1220';
+const summaryServer = 'http://localhost:1220';
 const aggServer = 'http://ec2-18-220-21-137.us-east-2.compute.amazonaws.com:2880';
 const alsoEnjoyedServer = 'http://ec2-35-162-103-218.us-west-2.compute.amazonaws.com:4000';
 
@@ -123,13 +123,16 @@ app.all('/reviews/*', (req, res) => {
     });
 });
 app.all('/api/summary/*', (req, res) => {
+  console.log('being accessed')
   const url = (summaryServer + req.path).trim();
+  console.log(url)
   console.log('proxying request to summary server with method', req.method, 'directed to', url);
   axios({
     method: req.method,
     url: url
   })
     .then((response) => {
+      console.log(response.data)
       res.send(JSON.stringify(response.data));
     });
 });
